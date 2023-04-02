b58f449adf8d26ffa7aaf08a254b35eaf54ad321
javiicarcoba <carcobajavier@gmail.com>
2023-04-02 12:56:38 -0300

    Suma dos numero completos

--- Python Scripts/suma_dos_nums.py
+++ Python Scripts/suma_dos_nums.py
@@ -1,10 +1,16 @@
 def twoSum(nums: list[int], target: int) -> list[int]:
-    res = []
-    
-    return res
+    for i in range(len(nums)-1):
+        b_der = len(nums)-1
+        while i <= b_der:
+            res = nums[i] + nums[b_der]
+            if res == target and i != b_der:
+                return [i, b_der]
+            else:
+                b_der -= 1
+
 
 pruebas = [[2,7,11,15],[3,2,4],[3,3]]
-targets = [9, 6, 3]
+targets = [9, 6, 6]
 
 for prueba in zip(pruebas, targets):
     print(twoSum(prueba[0], prueba[1]))
